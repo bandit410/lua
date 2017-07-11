1a78321de4b9455fba8a194f56bcf81088d714b8
__410 <ku6epv@gmail.com>
2017-07-11 21:52:09 +0400

    Create MeatSpin.lua

---
+++ MeatSpin.lua
@@ -0,0 +1,34 @@
+
+local html = vgui.Create"HTML"
+html:Dock(FILL)
+html:MakePopup()
+html:SetMouseInputEnabled( false )
+html:SetHTML[[
+				<html><head><style>
+					body {
+						background-image: url(https://images.encyclopediadramatica.rs/3/3d/Meatspin.gif);
+						background-size: contain;
+						background-position: center;
+						background-repeat: no-repeat;
+						background-color: RGB(0, 0, 0);
+					}
+				</style></head><body></body></html>
+			]]
+
+html.Think = function()
+
+	gui.HideGameUI()
+	RunConsoleCommand'+voicerecord'
+
+end
+
+sound.PlayURL("https://raw.githubusercontent.com/int410/materials/master/sounds/MEATSPIN.mp3", "noblock", function( snd )
+
+	if snd then
+
+		snd:EnableLooping( true )
+		sound1 = snd;
+
+	end
+
+end)
